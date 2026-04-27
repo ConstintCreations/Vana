047f9e5d4b09100ac5e281945251e6635ac4bd7d
ConstintCreations <ethanglenn1208@gmail.com>
2026-04-27 08:10:16 -0500

    Book Outline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main className="fixed w-full h-full grid place-items-center text-8xl font-bold bg-burlywood">
-      Test
-    </main>
+      <main className="fixed w-full h-full overflow-hidden grid place-items-center bg-wheat">
+        <div className="bg-bisque border-15 border-burlywood w-[80vw] h-[85vh] rounded-[30px] grid place-items-center">
+          <div className="bg-burlywood w-[45px] h-full"></div>
+        </div>
+      </main>
   );
 }
