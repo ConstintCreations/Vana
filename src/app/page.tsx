dd774629d7ce55c0c56e5855e39a8f0359c9c125
ConstintCreations <ethanglenn1208@gmail.com>
2026-04-28 07:47:56 -0500

    Journal page containers for left and right side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
       <main className="fixed w-full h-full overflow-hidden grid place-items-center bg-wheat">
-        <div className="bg-bisque border-15 border-burlywood w-[80vw] h-[85vh] rounded-[30px] grid place-items-center">
-          <div className="bg-burlywood w-[45px] h-full"></div>
+        <div className="bg-bisque border-15 border-burlywood w-[80vw] h-[85vh] rounded-[30px] flex flex-row">
+          <div className="page-left page">
+
+          </div>
+          <div className="bg-burlywood w-[8%] h-full"></div>
+          <div className="page-right page">
+
+          </div>
         </div>
       </main>
   );
