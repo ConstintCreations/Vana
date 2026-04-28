e9f65e4d22b5b1f77bc833abc0399893e6db91de
ConstintCreations <ethanglenn1208@gmail.com>
2026-04-28 08:05:13 -0500

    Created a SectionButton component

---
+++ src/app/components/sectionButton.tsx
@@ -0,0 +1,7 @@
+export default function SectionButton() {
+    return (
+        <button className="bg-saddlebrown w-20 h-20 rounded-b-4xl outline-4 outline-saddlebrown border-burlywood border-4 border-dashed p-3 cursor-pointer transition-colors duration-300 hover:bg-sienna hover:outline-sienna hover:border-bisque">
+            
+        </button>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+import SectionButton from "./components/sectionButton";
+
 export default function Home() {
   return (
       <main className="fixed w-full h-full overflow-hidden grid place-items-center bg-wheat">
         <div className="bg-bisque border-15 border-burlywood w-[80vw] h-[85vh] rounded-[30px] flex flex-row">
-          <div className="page-left page">
-
+          <div className="page-left page flex flex-col">
+            <div className="flex flex-row-reverse py-[4px] px-5 gap-5">
+              <SectionButton />
+              <SectionButton />
+              <SectionButton />
+            </div>
           </div>
           <div className="bg-burlywood w-[8%] h-full"></div>
           <div className="page-right page">
