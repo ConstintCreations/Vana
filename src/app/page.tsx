13464ed81fa4634805746675064ecc50299e7d64
ConstintCreations <ethanglenn1208@gmail.com>
2026-04-29 08:11:24 -0500

    Tab buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,17 @@ export default function Home() {
           </div>
           <div className="bg-burlywood w-[8%] h-full"></div>
           <div className="page-right page">
-
+            <div className="flex flex-col h-[75vh] top-[12.5vh] fixed left-[calc(90vw-15px)] gap-[2vh] w-[4vw] justify-around align-end">
+              <div className="cursor-pointer w-full h-[30%] bg-bisque rounded-tr-[30px] rounded-br-[30px] border-burlywood border-r-15 border-t-15 border-b-15 flex justify-end items-center flex-nowrap">
+                <p className="rotate-90 text-burlywood font-bold text-2xl">Main</p>
+              </div>
+              <div className="cursor-pointer w-full h-[30%] bg-wheat rounded-tr-[30px] rounded-br-[30px] border-burlywood border-r-15 border-t-15 border-b-15 flex justify-end items-center">
+                <p className="rotate-90 text-burlywood font-bold text-2xl -mr-6">Settings</p>
+              </div>
+              <div className="cursor-pointer w-full h-[30%] bg-wheat rounded-tr-[30px] rounded-br-[30px] border-burlywood border-r-15 border-t-15 border-b-15 flex justify-end items-center">
+                <p className="rotate-90 text-burlywood font-bold text-2xl">Help</p>
+              </div>
+            </div>
           </div>
         </div>
       </main>
