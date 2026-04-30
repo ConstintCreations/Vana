ac5ed369c56b14e9c610a49512cf2072f3bc6904
ConstintCreations <ethanglenn1208@gmail.com>
2026-04-30 07:53:47 -0500

    Section button onClick

--- src/app/components/sectionButton.tsx
+++ src/app/components/sectionButton.tsx
@@ -1,9 +1,10 @@
-import { FontAwesomeIcon, FontAwesomeIconProps } from "@fortawesome/react-fontawesome"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { IconDefinition } from "@fortawesome/free-solid-svg-icons";
 
-export default function SectionButton({icon}: FontAwesomeIconProps) {
+export default function SectionButton({icon, onClick}: {icon: IconDefinition, onClick: () => void}) {
     return (
-        <button className="bg-saddlebrown size-17 rounded-b-4xl outline-4 outline-saddlebrown border-burlywood border-4 border-dashed p-3 cursor-pointer transition-colors duration-300 hover:bg-sienna hover:outline-sienna hover:border-bisque grid place-items-center group">
-            <FontAwesomeIcon className="size-9 text-burlywood group-hover:text-bisque transition-colors duration-300" icon={icon}></FontAwesomeIcon>
+        <button onClick={onClick} className="bg-saddlebrown size-17 rounded-b-4xl outline-4 outline-saddlebrown border-burlywood border-4 border-dashed p-3 cursor-pointer transition-colors duration-300 hover:bg-sienna hover:outline-sienna hover:border-bisque grid place-items-center group">
+            <FontAwesomeIcon style={{width: "36px", height: "36px"}} className="size-9 text-burlywood group-hover:text-bisque transition-colors duration-300" icon={icon}></FontAwesomeIcon>
         </button>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,29 @@
+"use client";
 import SectionButton from "./components/sectionButton";
 import TabButton from "./components/tabButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCalendarCheck, faClipboardList, faShop } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 export default function Home() {
+
+  const [section, setSection] = useState<"Daily" | "Summary" | "Shop">("Daily");
+
   return (
       <main className="fixed w-full h-full overflow-hidden grid place-items-center bg-wheat">
         <div className="bg-bisque border-15 border-burlywood w-[80vw] h-[85vh] rounded-[30px] flex flex-row">
           <div className="page-left page flex flex-col">
             <div className="flex flex-row justify-between py-[4px] px-5 gap-5">
               <h1 className="font-bold text-saddlebrown mt-5 text-4xl flex-1 flex justify-center">
-                Summary
+                {section}
               </h1>
               <div className="flex flex-row gap-5">
                 <div className="flex flex-row gap-5">
-                  <SectionButton icon={faCalendarCheck} /> {/*Daily*/}
-                  <SectionButton icon={faClipboardList} /> {/*Summary*/}
+                  <SectionButton icon={faCalendarCheck} onClick={() => setSection("Daily")} /> {/*Daily*/}
+                  <SectionButton icon={faClipboardList} onClick={() => setSection("Summary")} /> {/*Summary*/}
                 </div>
                 <div className="w-[1px] outline-4 outline-saddlebrown h-full bg-saddlebrown rounded-b-full"></div>
-                <SectionButton icon={faShop} /> {/*Shop*/}
+                <SectionButton icon={faShop} onClick={() => setSection("Shop")} /> {/*Shop*/}
               </div>
             </div>
             <div className="h-[6px] mt-3 w-full bg-saddlebrown">
