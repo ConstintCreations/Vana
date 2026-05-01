4f18372fd746a337ee9efde7a1b676586dfa0228
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-01 15:23:10 -0500

    Summary type button size change on selected

--- src/app/components/summaryTypeButton.tsx
+++ src/app/components/summaryTypeButton.tsx
@@ -1,5 +1,5 @@
 export default function SummaryTypeButton({text, selected, onClick} : {text: string, selected: Boolean, onClick: () => void}) {
     return(
-        <button onClick={onClick} className={`${ selected ? "text-saddlebrown underline hover:text-sienna" : "text-saddlebrown opacity-60 hover:opacity-90"} transition-all duration-400 font-bold cursor-pointer decoration-3 underline-offset-7 [text-decoration-skip-ink:none]`}>{text}</button>
+        <button onClick={onClick} className={`${ selected ? "text-saddlebrown underline hover:text-sienna text-2xl" : "text-saddlebrown opacity-60 hover:opacity-90 text-xl"} transition-all duration-400 font-bold cursor-pointer decoration-3 underline-offset-7 [text-decoration-skip-ink:none]`}>{text}</button>
     );
 }
