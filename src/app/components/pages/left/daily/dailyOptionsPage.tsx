4a77f7338573b49e7d7a671bf0ff29232ecf347a
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-03 09:20:48 -0500

    Created an add a habit form with an input for the habit name on the daily options page

--- src/app/components/pages/left/daily/dailyOptionsPage.tsx
+++ src/app/components/pages/left/daily/dailyOptionsPage.tsx
@@ -1,8 +1,12 @@
 export default function DailyOptionsPage() {
 
     return (
-        <div>
-            
-        </div>
+        <form className="mt-5 flex flex-col justify-center items-center">
+            <h1 className="text-2xl font-bold mb-5">Add a Habit</h1>
+            <div className="flex flex-row gap-5 justify-between w-[80%] items-center">
+                <label className="font-bold text-xl">Habit</label>
+                <input className="font-bold border-3 outline-none border-saddlebrown bg-burlywood focus:border-burlywood rounded-full py-1 px-4 text-center flex-1 focus:bg-wheat focus:scale-[1.05] transition-all duration-300" type="text"></input>
+            </div>
+        </form>
     );
 }
