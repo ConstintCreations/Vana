9044735e3d944de8c61d3b88f0d75bc0dd225e44
ConstintCreations <ethanglenn1208@gmail.com>
2026-05-03 09:40:47 -0500

    Added appleslice to color palette and changed add a habit input colors

--- src/app/components/pages/left/daily/dailyOptionsPage.tsx
+++ src/app/components/pages/left/daily/dailyOptionsPage.tsx
@@ -5,7 +5,7 @@ export default function DailyOptionsPage() {
             <h1 className="text-2xl font-bold mb-5">Add a Habit</h1>
             <div className="flex flex-row gap-5 justify-between w-[80%] items-center">
                 <label className="font-bold text-xl">Habit</label>
-                <input className="font-bold border-3 outline-none border-saddlebrown bg-burlywood focus:border-burlywood rounded-full py-1 px-4 text-center flex-1 focus:bg-wheat focus:scale-[1.05] transition-all duration-300" type="text"></input>
+                <input className="font-bold border-3 outline-none border-appleslice bg-wheat focus:border-burlywood focus:bg-moccasin rounded-full py-1 px-4 text-center flex-1 focus:scale-[1.05] transition-all duration-300" type="text"></input>
             </div>
         </form>
     );
--- src/app/components/pages/right/daily/dailyOptionsPageRight.tsx
+++ src/app/components/pages/right/daily/dailyOptionsPageRight.tsx
@@ -4,14 +4,13 @@ export default function DailyOptionsPageRight() {
         <div className="flex flex-row flex-wrap">
             <div className="w-[25%] aspect-square bg-saddlebrown grid place-items-center font-bold">saddlebrown</div>
             <div className="w-[25%] aspect-square bg-sienna grid place-items-center font-bold">sienna</div>
+            <div className="w-[25%] aspect-square bg-appleslice grid place-items-center font-bold">appleslice</div>
             <div className="w-[25%] aspect-square bg-tan grid place-items-center font-bold">tan</div>
             <div className="w-[25%] aspect-square bg-burlywood grid place-items-center font-bold">burlywood</div>
-
             <div className="w-[25%] text-black aspect-square bg-wheat grid place-items-center font-bold">wheat</div>
             <div className="w-[25%] text-black aspect-square bg-moccasin grid place-items-center font-bold">moccasin</div>
             <div className="w-[25%] text-black aspect-square bg-bisque grid place-items-center font-bold">bisque</div>
             <div className="w-[25%] text-black aspect-square bg-blanchedalmond grid place-items-center font-bold">blanchedalmond</div>
-            
             <div className="w-[25%] text-black aspect-square bg-antiquewhite grid place-items-center font-bold">antiquewhite</div>
             <div className="w-[25%] text-black aspect-square bg-papayawhip grid place-items-center font-bold">papayawhip</div>
             <div className="w-[25%] text-black aspect-square bg-beige grid place-items-center font-bold">beige</div>
